a839c6e0f67a38e1c06cedcfc2eccf935e145354
Satyanarayan Kanth <shyamkanth0805@gmail.com>
2023-08-26 17:47:15 +0530

    Queue STL implementation added

--- Linked List/Circular linked list/implementation.cpp
+++ Linked List/Circular linked list/implementation.cpp
@@ -12,11 +12,40 @@ class Node{
         this->next = NULL;
     }
 
+
+    void insertion(Node* &head,int element, int val){
+        Node* newNode = new Node(val);
+        if(head == NULL){
+            head = newNode;
+            newNode->next = newNode;
+            return;
+        }
+        Node* curr = head;
+        while(curr->data != element){
+            curr = curr->next;
+        }
+        newNode->next = curr->next;
+        curr->next = newNode;
+    }
+
+    void print(Node* head){
+        Node* temp = head;
+        do{
+            cout<<head->data<<" ";
+            head = head->next;
+        }while(head != temp);
+        cout<<endl;
+    }
+
 };
+
 
 int main()
 {
-    Node* tail = new Node(10);
-    cout<<tail->data;
+    Node* op;
+    Node* head = NULL;
+    op->insertion(head,1,3);
+    op->insertion(head,3,5);
+    op->print(head);
     return 0;
 }
