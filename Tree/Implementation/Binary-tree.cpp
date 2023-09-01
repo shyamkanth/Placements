62c06fad0a60253771afe344b99bc52c1d1ace26
Satyanarayan Kanth <shyamkanth0805@gmail.com>
2023-09-01 11:42:26 +0530

    Binary Tree implementation

---
+++ Tree/Implementation/Binary-tree.cpp
@@ -0,0 +1,32 @@
+// Creation of Binary tree
+// Shyam Sunder Kanth
+// Insta : still_23.6_8
+
+#include<iostream>
+
+using namespace std;
+
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int data){
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+int main()
+{
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    cout<<root->data<<endl;
+    cout<<root->left->data<<endl;
+    cout<<root->right->data<<endl;
+
+    return 0;
+}
